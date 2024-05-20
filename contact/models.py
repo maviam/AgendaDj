0ac2fa2526fe817f8e8c0c5a0226e977663025c7
Marcelo Amorim <marceloamorim.fa@gmail.com>
2024-05-20 19:21:17 +0100

    Owner Field

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -34,6 +35,7 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True,upload_to='pictures/%Y/%m')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
     
     def __str__(self):
         return f'{self.first_name} {self.last_name} ({self.email})'
