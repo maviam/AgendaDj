93eab67d89e6eb47ab4237d493bfa23bb3e93ee1
Marcelo Amorim <marceloamorim.fa@gmail.com>
2024-05-28 10:32:45 +0100

    Create form and test erros in fields and non-fields

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,22 +2,52 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from contact.models import Contact
 from django.core.paginator import Paginator
+from django import forms
+from django.core.exceptions import ValidationError
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('first_name','last_name','phone',)
+    
+    def clean(self):
+        # Para mostrar que a classe tem acesso aos dados dos campos antes da submissão ...
+        cleaned_data = self.cleaned_data # Guarda os valores inseridos em um dicionário
+        print(cleaned_data)
+        # Vamos simular erros
+        self.add_error(
+			None,
+			ValidationError(
+				'Dados em falta ou inválidos',
+				code='invalid'
+			)
+		)
+        # Vamos simular outro erro
+        self.add_error(
+			None,
+			ValidationError(
+				'Nova mensagem de erro',
+				code='invalid'
+			)
+		)
+        return super().clean()
 
 # Create your views here.
 def create(request):
-    if request.method == 'POST': # QUando enviamos os dados do formulário
-        print()
-        print(request.method)
-        # Para ver na consola que o texto escrito é "pego" pela instrução dentro do print
-        print(request.POST.get('first_name')) # first_name foi o nome dado ao input do formulário
+    if request.method == 'POST':
+        context = {
+		    'form': ContactForm(data=request.POST)
+	    }
+    
+        return render(
+	        request,
+	        'contact/create.html',
+		    context
+	    )
     
     context = {
-		
+		'form': ContactForm()
 	}
-    
-    # Método GET (quando acedemos a página create,html)
-    print()
-    print(request.method)
     
     return render(
 	    request,
